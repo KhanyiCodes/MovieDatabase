a73acf9116010e3528ae782993faaab0e2350caf
KhanyiCodes <rebaone02khanyi@gmail.com>
2025-01-10 16:07:42 +0200

    Deleted a few files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Home from './components/Home';
-import GenreMenu from './components/GenreMenu';
-import Search from './components/Search';
-import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="app">
-        <Navbar />
-        <GenreMenu />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/genre/:id" element={<Home />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
